4db21faf86d1921df78bdf944be5308597f6355e
xiaoyang.cui <xiaoyang.cui@tec-do.com>
2025-06-18 19:58:55 +0800

    跨域配置bugfix

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ import router from './routes/index.js'; // 引入路由模块
 const app = express();
 const port = 8000;
 
-import { userRouter } from './routes/用户.js';
 
 // 请求头跨域配置
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // 上线后只允许配置为前端ip端口
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+const cors = require('cors');
+app.use(cors({
+  origin: '*', // 生产环境建议指定前端域名
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 
 // 请求json处理拦截器
 app.use(express.json());
-app.use('/user', userRouter);
-// router(app);
+router(app);
 
 // 启动服务器
 app.listen(port, () => {

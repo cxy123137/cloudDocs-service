7e3a7218bc35e1fb89ef247e32e91fbcfa722e2a
xiaoyang.cui <xiaoyang.cui@tec-do.com>
2025-06-19 23:16:44 +0800

    文档接口完善

--- routes/doc.js
+++ routes/doc.js
@@ -6,17 +6,35 @@ const documentsrouter = express.Router();
 // 查询文档
 documentsrouter.get('/getDoc', async (req, res) => {
   try {
-    const docs = await getDocument(req);
+    const { id } = req.query;
+    const docs = await getDocument({ id });
     res.status(200).json(docs);
   } catch (error) {
     res.status(500).send(error.toString());
   }
 });
 
-// 插入文档
+// 新建文档
 documentsrouter.post('/addDoc', async (req, res) => {
   try {
-    const result = await addDocument(req);
+    const {
+      title,
+      baseId,
+      content,
+      version,
+      snapshotAtVersion,
+      snapshot,
+      valid,
+    } = req.body;
+    const result = await addDocument({
+      title,
+      baseId,
+      content,
+      version,
+      snapshotAtVersion,
+      snapshot,
+      valid,
+    });
     res.status(201).json({ message: 'Document inserted successfully', insertedId: result.insertedId });
   } catch (error) {
     res.status(500).send(error.toString());
@@ -26,8 +44,13 @@ documentsrouter.post('/addDoc', async (req, res) => {
 // 更新文档
 documentsrouter.put('/put/:id', async (req, res) => {
   try {
-    req.body._id = req.params.id; // 将文档ID添加到请求体中
-    const result = await updateDocument(req);
+    const { id } = req.params;
+    const { content, valid } = req.body;
+    const result = await updateDocument({
+      id,
+      content,
+      valid,
+    });
     if (result.matchedCount === 1) {
       res.status(200).json({ message: 'Document updated successfully' });
     } else {
@@ -41,8 +64,8 @@ documentsrouter.put('/put/:id', async (req, res) => {
 // 删除文档
 documentsrouter.delete('/delete/:id', async (req, res) => {
   try {
-    req.body._id = req.params.id; // 将文档ID添加到请求体中
-    const result = await deleteDocument(req);
+    const { id } = req.params;
+    const result = await deleteDocument({ id });
     if (result.deletedCount === 1) {
       res.status(200).json({ message: 'Document deleted successfully' });
     } else {
--- service/doc.js
+++ service/doc.js
@@ -1,49 +1,15 @@
 import { connectToDatabase } from '../db.js';
 import { ObjectId } from 'mongodb';
 
+// 连接到数据库
 const { db } = await connectToDatabase();
 
-// 新增和保存文档，都是新生成一个文档快照版本，两个接口都调用这个方法
-export async function addDocument(req) {
-  const newDoc = {
-    _id: new ObjectId(),
-    title: req.body.title || "未命名文档",
-    baseId: req.body.baseId,
-    content: req.body.content || {},
-    version: req.body.version || 0,
-    snapshotAtVersion: req.body.snapshotAtVersion || null,
-    snapshot: req.body.snapshot || '',
-    valid: req.body.valid || 1,
-    createTime: new Date(),
-    updateTime: new Date(),
-  };
-  return await performDatabaseOperation(db.collection('docs').insertOne(newDoc));
-}
-
-export async function getDocument(req) {
-  let documents;
-  if (req.query.id) {
-    documents = await db.collection('docs').findOne({_id: new ObjectId(req.query.id), valid: 1});
-  } else {
-    documents = await db.collection('docs').find({valid: 1}).toArray();
+// 生成新的 ObjectId
+function generateObjectId(id) {
+  if (id) {
+    return new ObjectId(id);
   }
-  return await performDatabaseOperation(documents);
-}
-
-// 修订历史版本使用
-export async function updateDocument(req) {
-  const documentData = {
-    content: req.body.content || {},
-    valid: req.body.valid || 1,         // 删除使用
-    updateTime: new Date(),
-  };
-  const result = await db.collection('docs').updateOne({_id: new ObjectId(req.body._id), valid: 1}, {$set: documentData});
-  return await performDatabaseOperation(result);
-}
-
-export async function deleteDocument(req) {
-  const result = await db.collection('docs').deleteOne({_id: new ObjectId(req.body._id), valid: 1});
-  return await performDatabaseOperation(result);
+  return new ObjectId();
 }
 
 // 通用的数据库操作辅助函数
@@ -53,6 +19,66 @@ async function performDatabaseOperation(operation) {
     return result;
   } catch (error) {
     console.error('Database operation failed:', error);
-    throw error; // 抛出错误以便调用者可以适当地处理它
+    throw error;
   }
+}
+
+// 新增文档
+export async function addDocument(title = "未命名文档", baseId, content = {}, 
+  version = 0, snapshotAtVersion = null, snapshot = '', valid = 1,) {
+  const newDoc = {
+    _id: generateObjectId(),
+    title,
+    baseId: generateObjectId(baseId),
+    content,
+    version,
+    snapshotAtVersion,
+    snapshot,
+    valid,
+    createTime: new Date(),
+    updateTime: new Date(),
+  };
+  const result = await performDatabaseOperation(db.collection('docs').insertOne(newDoc));
+  return result;
+}
+
+// 查询文档
+export async function getDocument({ id } = {}) {
+  if (id) {
+    return await performDatabaseOperation(
+      db.collection('docs').findOne({ _id: generateObjectId(id), valid: 1 })
+    );
+  } else {
+    return await performDatabaseOperation(
+      db.collection('docs').find({ valid: 1 }).toArray()
+    );
+  }
+}
+
+// 更新文档
+export async function updateDocument({
+  id,
+  content = {},
+  valid = 1,
+}) {
+  const documentData = {
+    content,
+    valid,
+    updateTime: new Date(),
+  };
+  const result = await performDatabaseOperation(
+    db.collection('docs').updateOne(
+      { _id: generateObjectId(id), valid: 1 },
+      { $set: documentData }
+    )
+  );
+  return result;
+}
+
+// 删除文档
+export async function deleteDocument(id) {
+  const result = await performDatabaseOperation(
+    db.collection('docs').deleteOne({ _id: generateObjectId(id), valid: 1 })
+  );
+  return result;
 }

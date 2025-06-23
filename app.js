19cb3c25bb06efd44470cf700eb378ad317082e8
xiaoyang.cui <xiaoyang.cui@tec-do.com>
2025-06-23 23:43:15 +0800

    refresh修改

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 // 全局拦截请求，必须登录
 app.use((req, res, next) => {
   // 白名单
-  if (req.path === '/login/login' || req.path === '/login/register' || req.path === '/login//refreshToken') {
+  if (req.path === '/login/login' || req.path === '/login/register' || req.path === '/login/refreshToken') {
     return next();
   }
 
